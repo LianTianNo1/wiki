5358d9d04785fe3cb7cfca5c145a694e391029e6
田浪 <1584731441@qq.com>
2021-07-08 16:24:02 +0800

    3-5 集成Mybaits-generator

--- src/main/java/com/xxxx/wiki/config/WikiApplication.java
+++ src/main/java/com/xxxx/wiki/config/WikiApplication.java
@@ -1,5 +1,6 @@
 package com.xxxx.wiki.config;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -7,8 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
-@ComponentScan("com.xxxx")
 @SpringBootApplication
+@ComponentScan("com.xxxx")
+@MapperScan("com.xxxx.wiki.mapper")
 public class WikiApplication {
 
     private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
