60c504b94aca27592796d65704cef21b3bb78334
田浪 <1584731441@qq.com>
2021-07-08 13:13:22 +0800

    2-7 增加hello接口测试test.http

--- src/main/java/com/xxxx/wiki/controller/TestController.java
+++ src/main/java/com/xxxx/wiki/controller/TestController.java
@@ -1,8 +1,7 @@
 package com.xxxx.wiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,5 +15,10 @@ public class TestController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World! Post, " + name;
     }
 }
