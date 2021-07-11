0c63a31020d4534d9c56927bc48d6f3755fd7a59
田浪 <1584731441@qq.com>
2021-07-11 15:16:18 +0800

    4-7 制作the-footer组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,20 +2,20 @@
   <a-layout>
     <the-header></the-header>
     <router-view/>
-    <a-layout-footer style="text-align: center">
-      Wzzzy's Wiki ©2021 Created by STFU
-    </a-layout-footer>
+    <the-footer></the-footer>
   </a-layout>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue';
+  import { defineComponent } from 'vue'
+  import TheFooter from '@/components/the-footer.vue'
   import TheHeader from '@/components/the-header.vue'
 
   export default defineComponent({
     name: 'app',
     components: {
-      TheHeader
+      TheHeader,
+      TheFooter
     }
   });
 </script>
---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <a-layout-footer style="text-align: center">
+        Wzzzy's Wiki ©2021 Created by STFU
+    </a-layout-footer>
+</template>
+
+<script>
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: 'the-footer'
+    });
+</script>
+
+<style scoped>
+
+</style>
