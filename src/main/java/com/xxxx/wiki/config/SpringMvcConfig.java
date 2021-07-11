efafcd9938d3d7a12e701d51663d031eae7d1501
田浪 <1584731441@qq.com>
2021-07-12 00:33:10 +0800

    5-9 增加AOP，打印接口耗时、请求参数和返回参数 去掉过滤器和拦截器

--- src/main/java/com/xxxx/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/xxxx/wiki/config/SpringMvcConfig.java
@@ -1,14 +1,14 @@
-package com.xxxx.wiki.config;
-
-import com.xxxx.wiki.interceptor.LogInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
+//package com.xxxx.wiki.config;
+//
+//import com.xxxx.wiki.interceptor.LogInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import javax.annotation.Resource;
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
 //
 //    @Resource
 //    LoginInterceptor loginInterceptor;
@@ -36,12 +36,12 @@ public class SpringMvcConfig implements WebMvcConfigurer {
 //                        "/*/save",
 //                        "/*/delete/**",
 //                        "/*/reset-password");
-    @Resource
-    LogInterceptor logInterceptor;
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(logInterceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns("/login");
-    }
-}
+//    @Resource
+//    LogInterceptor logInterceptor;
+//
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(logInterceptor)
+//                .addPathPatterns("/**")
+//                .excludePathPatterns("/login");
+//    }
+//}
