4f907042548cac747a0ebf8309200d57494a8096
田浪 <1584731441@qq.com>
2021-07-13 00:49:39 +0800

    6-2 增加admin-ebook.vue

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,15 +1,15 @@
 <template>
-    <a-layout-footer style="text-align: center">
-        Wzzzy's Wiki ©2021 Created by STFU
-    </a-layout-footer>
+  <a-layout-footer style="text-align: center">
+      Wzzzy's Wiki ©2021 Created by STFU
+  </a-layout-footer>
 </template>
 
 <script>
-    import { defineComponent } from 'vue';
+  import { defineComponent } from 'vue';
 
-    export default defineComponent({
-        name: 'the-footer'
-    });
+  export default defineComponent({
+      name: 'the-footer'
+  });
 </script>
 
 <style scoped>
---
+++ web/src/views/admin/admin-ebook.vue
@@ -0,0 +1,9 @@
+<template>
+  <a-layout-content
+    :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+  >
+    <div class="about">
+      <h1>电子书管理</h1>
+    </div>
+  </a-layout-content>
+</template>
