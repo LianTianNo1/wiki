90e21c8eb849afa40dd846647706c8d2838243a8
田浪 <1584731441@qq.com>
2021-07-13 22:26:13 +0800

    6-12 增加Tool.ts，使用其中的copy方法

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -74,6 +74,7 @@
   import {defineComponent, onMounted, ref} from 'vue';
   import axios from 'axios';
   import {message} from "ant-design-vue";
+  import {Tool} from "@/util/tool";
 
   export default defineComponent({
     name: 'AdminEbook',
@@ -192,7 +193,7 @@
        */
       const edit = (record: any) => {
         modalVisible.value = true
-        ebook.value = record
+        ebook.value = Tool.copy(record)
       }
 
       /**
