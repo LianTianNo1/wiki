90944c18155dba7adf33fe2bff9c3b471f31b151
田浪 <1584731441@qq.com>
2021-07-22 21:15:24 +0800

    9-9 集成redis，登录成功后将信息放入redis中

--- src/main/java/com/xxxx/wiki/controller/UserController.java
+++ src/main/java/com/xxxx/wiki/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.xxxx.wiki.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.xxxx.wiki.req.UserLoginReq;
 import com.xxxx.wiki.req.UserQueryReq;
 import com.xxxx.wiki.req.UserResetPasswordReq;
@@ -9,11 +10,16 @@ import com.xxxx.wiki.resp.PageResp;
 import com.xxxx.wiki.resp.UserLoginResp;
 import com.xxxx.wiki.resp.UserQueryResp;
 import com.xxxx.wiki.service.UserService;
+import com.xxxx.wiki.util.SnowFlake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by stfu
@@ -25,6 +31,14 @@ public class UserController {
 
     @Resource
     private UserService userService;
+
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    @Resource
+    private SnowFlake snowFlake;
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserService.class);
 
     @GetMapping("/list")
     public CommonResp list(@Valid UserQueryReq req) {
@@ -64,6 +78,15 @@ public class UserController {
         //  对明文密码进行加密
         req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         UserLoginResp userLoginResp = userService.login(req);
+
+        //  通过雪花算法生成token
+        Long token = snowFlake.nextId();
+        LOG.info("生成单点登录token: {}，并放入redis中", token);
+        //  将token返回给前端
+        userLoginResp.setToken(token.toString());
+        //  将token放入redis
+        //  存入redis的类需要实现序列化 或者 JSONObject.toJSONString()
+        redisTemplate.opsForValue().set(token, JSONObject.toJSONString(userLoginResp), 3600 * 24, TimeUnit.SECONDS);
 
         CommonResp<UserLoginResp> resp = new CommonResp<>();
         resp.setContent(userLoginResp);
