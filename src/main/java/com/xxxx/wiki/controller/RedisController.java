4f3a894c6d3ec46281d696b6437ede61aa7ec1db
田浪 <1584731441@qq.com>
2021-07-22 22:17:53 +0800

    9-9 增加了redis测试接口

---
+++ src/main/java/com/xxxx/wiki/controller/RedisController.java
@@ -0,0 +1,34 @@
+package com.xxxx.wiki.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
+
+@RestController
+public class RedisController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RedisController.class);
+
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    @RequestMapping("/redis/set/{key}/{value}")
+    public String set(@PathVariable Long key, @PathVariable String value) {
+        redisTemplate.opsForValue().set(key, value, 3600, TimeUnit.SECONDS);
+        LOG.info("key: {}, value: {}", key, value);
+        return "success";
+    }
+
+    @RequestMapping("/redis/get/{key}")
+    public Object get(@PathVariable Long key) {
+        Object object = redisTemplate.opsForValue().get(key);
+        LOG.info("key: {}, value: {}", key, object);
+        return object;
+    }
+}
