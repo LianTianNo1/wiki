64ba85ed3a50a7e2a9a0f902053e4092ea4cb3d9
田浪 <1584731441@qq.com>
2021-07-23 13:09:53 +0800

    10-6 增加定时器，定时执行电子书信息更新SQL，增加日志

--- src/main/java/com/xxxx/wiki/job/DocJob.java
+++ src/main/java/com/xxxx/wiki/job/DocJob.java
@@ -11,17 +11,20 @@ import javax.annotation.Resource;
 @Component
 public class DocJob {
 
-   private static final Logger LOG = LoggerFactory.getLogger(DocJob.class);
+    private static final Logger LOG = LoggerFactory.getLogger(DocJob.class);
 
-   @Resource
-   private DocService docService;
+    @Resource
+    private DocService docService;
 
     /**
      * 每30秒更新电子书信息
      */
-   @Scheduled(cron = "5/30 * * * * ?")
-   public void cron() {
-       docService.updateEbookInfo();
-   }
+    @Scheduled(cron = "5/30 * * * * ?")
+    public void cron() {
+        LOG.info("更新电子书下的文档数据开始");
+        long start = System.currentTimeMillis();
+        docService.updateEbookInfo();
+        LOG.info("更新电子书下的文档数据结束, 耗时: {}毫秒 ", System.currentTimeMillis() - start);
+    }
 
 }
