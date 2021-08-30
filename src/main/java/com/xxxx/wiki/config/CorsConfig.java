aa1ff4e69a80e006f416f58b5f4bd95a6d183158
田浪 <1584731441@qq.com>
2021-08-31 02:14:45 +0800

    去掉数据库和redis配置

--- src/main/java/com/xxxx/wiki/config/CorsConfig.java
+++ src/main/java/com/xxxx/wiki/config/CorsConfig.java
@@ -7,16 +7,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-
     //  跨域设置
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+                .allowedOrigins("*")
                 .allowedHeaders(CorsConfiguration.ALL)
                 .allowedMethods(CorsConfiguration.ALL)
                 .allowCredentials(true)
                 .maxAge(3600); // 1小时内不需要再预检（发OPTIONS请求）
     }
-
 }
+
